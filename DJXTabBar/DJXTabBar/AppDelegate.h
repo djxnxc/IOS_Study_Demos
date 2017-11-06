c6e8cd77193a882b66eed5ceb379801b232547ce
12 <1402017233@qq.com>
2017-11-06 11:10:41 +0800

    初建tabBar

---
+++ DJXTabBar/DJXTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJXTabBar
+//
+//  Created by 12 on 2017/11/6.
+//  Copyright © 2017年 Jack_D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
