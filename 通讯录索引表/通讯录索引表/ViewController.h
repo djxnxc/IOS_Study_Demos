ff8f6f8102a8cf2cb9495821c067c688be3399be
12 <1402017233@qq.com>
2017-11-06 10:08:13 +0800

    初建通讯录索引表

---
+++ 通讯录索引表/通讯录索引表/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  通讯录索引表
+//
+//  Created by 12 on 2017/11/6.
+//  Copyright © 2017年 Jack_D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 通讯录索引表/通讯录索引表/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  通讯录索引表
+//
+//  Created by 12 on 2017/11/6.
+//  Copyright © 2017年 Jack_D. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
